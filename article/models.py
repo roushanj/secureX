01277dac0d0fb6abd5e9e391984b972aac4b2713
roushanj <roushan,jha0709@gmail.com>
2016-10-29 15:19:38 +0530

    created a layout of sqldb

--- article/models.py
+++ article/models.py
@@ -6,7 +6,8 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
